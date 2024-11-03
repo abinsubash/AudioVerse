7050a71212c746d029a517b211e958c89238dec2
abin subash <abinsubashkk@gmail,com>
2024-11-03 15:06:52 +0530

    Passport

--- config/passport.js
+++ config/passport.js
@@ -5,7 +5,7 @@ const User = require('../model/userModel');
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,  
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: process.env.GOOGLE_CALLBACK_URL
+    callbackURL: "https://audioverse.store/auth/google/callback"
   },
   async (accessToken, refreshToken, profile, done) => { 
     try {
